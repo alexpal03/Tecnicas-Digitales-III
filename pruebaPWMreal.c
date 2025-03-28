42c60e04a6f6fb048b97d63db22673a4683b0825
Alex Palacios <alexsebpalacios@hotmail.com>
2025-03-28 20:26:28 -0300

    Update pruebaPWMreal.c

--- pruebaPWMreal.c
+++ pruebaPWMreal.c
@@ -3,13 +3,7 @@
 // 'C' source line config statements
 
 // CONFIG
-#pragma config FOSC = INTRCIO   // Oscillator Selection bits (INTOSC oscillator: I/O function on GP4/OSC2/CLKOUT pin, I/O function on GP5/OSC1/CLKIN)
-#pragma config WDTE = ON        // Watchdog Timer Enable bit (WDT enabled)
-#pragma config PWRTE = ON       // Power-Up Timer Enable bit (PWRT enabled)
-#pragma config MCLRE = ON       // GP3/MCLR pin function select (GP3/MCLR pin function is MCLR)
-#pragma config BOREN = ON       // Brown-out Detect Enable bit (BOD enabled)
-#pragma config CP = OFF         // Code Protection bit (Program Memory code protection is disabled)
-#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config WDTE=OFF , BOREN=OFF , PWRTE=ON , MCLRE=OFF , FOSC=INTRCIO
 
 // #pragma config statements should precede project file includes.
 // Use project enums instead of #define for ON and OFF.
@@ -37,7 +31,7 @@ void main(void)
 
 
 
-void interrupt isr()     //Función para la interrupción por Timer
+void __interrupt() isr()     //Función para la interrupción por Timer
 {
     if(INTCONbits.T0IF == 1)  
     {       
