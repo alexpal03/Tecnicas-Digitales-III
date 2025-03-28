323406a4ff419010fc9d692ee2b038e9e2ac55cd
MaxLRobertson <69312533+MaxLRobertson@users.noreply.github.com>
2025-03-28 19:21:33 -0300

    Create pruebaPWM.c

---
+++ pruebaPWM.c
@@ -0,0 +1,46 @@
+
+// PIC12F629 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG
+#pragma config FOSC = INTRCIO   // Oscillator Selection bits (INTOSC oscillator: I/O function on GP4/OSC2/CLKOUT pin, I/O function on GP5/OSC1/CLKIN)
+#pragma config WDTE = ON        // Watchdog Timer Enable bit (WDT enabled)
+#pragma config PWRTE = ON       // Power-Up Timer Enable bit (PWRT enabled)
+#pragma config MCLRE = ON       // GP3/MCLR pin function select (GP3/MCLR pin function is MCLR)
+#pragma config BOREN = ON       // Brown-out Detect Enable bit (BOD enabled)
+#pragma config CP = OFF         // Code Protection bit (Program Memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+
+#define LED1 GP5
+
+void main(void) {
+    
+    TRISIO = 0b00001100;
+    INTCON = 0x90;
+    OPTION_REG = 0b01001000;        //revisaar ultimos 4
+    
+    
+    while (1)
+    {
+       GP4 = !GP4;
+       __delay_ms(200);
+       
+    }    
+    return;
+}
+
+void __interrupt() ISR()
+{
+    if (INTCONbits.INTF == 1)
+    {
+        LED1 = !LED1;
+                
+    }
+    INTCONbits.INTF =0;
+}
